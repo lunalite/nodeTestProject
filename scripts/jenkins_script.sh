36de0dd6a78e312a6da71574f9bf9939dc752538
HD Koh <hd.koh290293@gmail.com>
2016-09-06 15:56:27 +0200

    Update jenkins_script.sh

--- scripts/jenkins_script.sh
+++ scripts/jenkins_script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "*** Building $IMAGE_NAME:$BUILD_NUMBER ..."
-docker build -t $IMAGE_NAME:$BUILD_NUMBER .
+docker build -t $IMAGE_NAME:$BUILD_NUMBER . --build-arg http_proxy=http://webproxy.merck.com:8080
 #docker push $IMAGE_NAME:$BUILD_NUMBER
 
 echo "*** Running compose ..."
