22ebf7830281575e1e8efdfa6638aa19493bbb41
luna <hd.koh290293@gmail.com>
2016-09-07 13:55:36 +0200

    shell

--- scripts/docker_command.sh
+++ scripts/docker_command.sh
@@ -3,20 +3,19 @@
 HOST=localhost
 PORT=27017
 
-#echo "Waiting for the DB ..." > test.log
-#while :
-#do
-#    (echo > /dev/tcp/$HOST/$PORT) >/dev/null 2>&1
-#    result=$?
-#    if [[ $result -eq 0 ]]
-#    then
-#        echo "DB is up!" >> test.log
-#        break
-#    fi
-#    echo "DB is down, waiting ..." >> test.log
-#    sleep 1
-#done
+echo "Waiting for the DB ..." > test.log
+while :
+do
+    (echo > /dev/tcp/$HOST/$PORT) >/dev/null 2>&1
+    result=$?
+    if [[ $result -eq 0 ]]
+    then
+        echo "DB is up!" >> test.log
+        break
+   fi
+    echo "DB is down, waiting ..." >> test.log
+    sleep 1
+done
 
-#echo "Executing ... " >> test.log
-npm run test-coverage 
-#>> test.log
+echo "Executing ... " >> test.log
+npm run test-coverage >> test.log
