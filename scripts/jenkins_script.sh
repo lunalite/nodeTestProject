06d0bc99e92f028924f1843bf3540b322d96db9b
luna <hd.koh290293@gmail.com>
2016-09-12 13:00:55 +0200

    docker compose

--- scripts/jenkins_script.sh
+++ scripts/jenkins_script.sh
@@ -5,17 +5,17 @@ docker build -t $IMAGE_NAME:$BUILD_NUMBER .
 #docker push $IMAGE_NAME:$BUILD_NUMBER
 
 echo "*** Running compose ..."
-docker-compose up
+docker-compose up -d
 TEST_RESULT=`docker wait $APP_NAME-$BUILD_NUMBER`
 
-#echo "*** Copying logs ..."
-#LOG_DATE=$(date +%F_%R)
-#docker cp $APP_NAME-$BUILD_NUMBER:/build/test.log ./test.log-${LOG_DATE}
-#cat ./test.log-${LOG_DATE}
+echo "*** Copying logs ..."
+LOG_DATE=$(date +%F_%R)
+docker cp $APP_NAME-$BUILD_NUMBER:/build/test.log ./test.log-${LOG_DATE}
+cat ./test.log-${LOG_DATE}
 
-#echo "*** Copying coverage files ..."
-#rm -r ./coverage
-#docker cp $APP_NAME-$BUILD_NUMBER:/build/coverage ./
+echo "*** Copying coverage files ..."
+rm -r ./coverage
+docker cp $APP_NAME-$BUILD_NUMBER:/build/coverage ./
 
 echo "*** Killing and removing compose ..."
 docker-compose kill && docker-compose rm -f
