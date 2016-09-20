ff31b9e70e28b5f673e844a9ff6588658b0ca854
HD Koh <hd.koh290293@gmail.com>
2016-09-20 11:24:57 +0200

    Update jenkins_script.sh

--- scripts/jenkins_script.sh
+++ scripts/jenkins_script.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 echo "*** Building $IMAGE_NAME:$BUILD_NUMBER ..."
+echo $http_proxy
+echo $HTTP_PROXY
 docker build -t $IMAGE_NAME:$BUILD_NUMBER . --build-arg WORKSPACE=$WORKSPACE --build-arg http_proxy=$http_proxy
 #docker push $IMAGE_NAME:$BUILD_NUMBER
 
